23dced8384b9beaf5109e3efd32a8caee75bc137
arikaran__r <152700924+arikaran03@users.noreply.github.com>
2024-08-09 14:51:53 +0530

    Time: 71 ms (82.34%), Space: 17.7 MB (76.48%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,57 +1,32 @@
-from typing import List
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        
-        if not nums:
-            return [-1,-1]
 
-        def binary(array,mid,left,right,check):
-            res = -2
+        def binary_search(nums,target,search):
+
+            left = 0
+            right = len(nums)-1
+            idx = -1
+
             while left<=right:
+
                 mid = (left+right)//2
 
-                if target == array[mid]:
-                    res = mid
-                    if check == True:
-                        left = mid+1
-                    else:
+                if nums[mid] == target:
+                    idx = mid
+
+                    if search:
+                        
                         right = mid-1
-
-                elif array[mid] <= target:
-                    left = mid+1
-                else:
+                    else:
+                        left = mid+1
+                elif nums[mid] > target:
+                    
                     right = mid-1
+                else:
 
-            
-            return res
-
-
-
-        left = 0
-        right = len(nums)-1
-        one = False
-        mid = 0
-        while left<=right:
-
-            mid = (left+right)//2
-
-            if nums[mid] == target:
-                one = True
-                break
-
-            if nums[mid] < target:
-                left = mid+1
-            else:
-                right = mid-1
-        if not one:
-            return [-1,-1]
-        else:
-            
-            leftval = binary(nums,mid,0,mid-1,False)
-            rightval = binary(nums,mid,mid+1,len(nums)-1,True)
-
-            if leftval == -2:
-                leftval = mid
-            if rightval == -2:
-                rightval = mid
-            return [leftval,rightval]
+                    left = mid+1
+            return idx
+        
+        leftval = binary_search(nums,target,True)
+        rightval = binary_search(nums,target,False)
+        return [leftval,rightval]
